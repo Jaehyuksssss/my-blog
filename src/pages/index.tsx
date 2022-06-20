52e7ae5dcddafcad8ba8023230c8b873ccd0a747
임재혁 <90457034+Jaehyuksssss@users.noreply.github.com>
2022-06-21 00:00:19 +0900

    Add files via upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import Footer from 'components/Common/Footer'
+import CategoryList from 'components/Main/CategoryLIst'
+import Introduction from 'components/Main/Introduction'
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
+
+const IndexPage: FunctionComponent = function () {
+  return (
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+      <Footer />
+    </Container>
+  )
+}
+
+export default IndexPage
---
+++ src/pages/info.tsx
@@ -0,0 +1,63 @@
+import React, { FunctionComponent } from 'react'
+import { graphql } from 'gatsby'
+import { Global, css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+
+type InfoPageProps = {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+        description: string
+        author: string
+      }
+    }
+  }
+}
+
+const TextStyle = css`
+  font-size: 18px;
+  font-weight: 700;
+  color: gray;
+`
+
+const InfoPage: FunctionComponent<InfoPageProps> = function ({
+  data: {
+    site: {
+      siteMetadata: { title, description, author },
+    },
+  },
+}) {
+  return (
+    <div>
+      <Global styles={globalStyle} />
+      <div css={TextStyle}>{title}</div>
+      {description} {author}
+    </div>
+  )
+}
+
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-size: 20px;
+  }
+`
+
+export default InfoPage
+
+export const metadataQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`
